d341e811ed97d77c95d93b8d59e18049786c8fb4
albertoornelasdev <albertoornelasdev@gmail.com>
2022-03-08 14:35:28 -0700

    Customer class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import models.Customer;
+
 public class Main {
     public static void main(String[] args) {
-        SystemTerminal systemTerminal = new SystemTerminal();
-        systemTerminal.startTerminal();
+
+        Customer customer = new Customer("juan petra", "12345", 14500.0);
+        System.out.println(customer.toString());
+//        SystemTerminal systemTerminal = new SystemTerminal();
+//        systemTerminal.startTerminal();
     }
 }
---
+++ src/models/Customer.java
@@ -0,0 +1,46 @@
+package models;
+
+public class Customer {
+    private String name;
+    private String id;
+    private double incomePerMoth;
+
+    public Customer(String name, String id, double incomePerMoth) {
+        this.name = name;
+        this.id = id;
+        this.incomePerMoth = incomePerMoth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getIncomePerMoth() {
+        return incomePerMoth;
+    }
+
+    public void setIncomePerMoth(double incomePerMoth) {
+        this.incomePerMoth = incomePerMoth;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+            "name='" + name + '\'' +
+            ", id='" + id + '\'' +
+            ", incomePerMoth=" + incomePerMoth +
+            '}';
+    }
+}
