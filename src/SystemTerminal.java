6574c5306e59b04bcf309825d05ef84835b23f8f
albertoornelasdev <albertoornelasdev@gmail.com>
2022-03-08 14:09:52 -0700

    System termianl to initi my terminal

--- src/SystemTerminal.java
+++ src/SystemTerminal.java
@@ -1,3 +1,71 @@
-public class SystemTerminal {
+import java.util.Objects;
 
+public class SystemTerminal {
+    private static final String PROP_USERNAME = "username";
+    private static final String PROP_PASSWORD = "password";
+    private static final byte TRY_LIMIT = 3;
+
+    // Function to start my terminal
+    protected void startTerminal() {
+        String username, password;
+        byte tryCount = 0;
+        boolean isLoggedIn;
+        System.out.println("Login...");
+        do {
+            System.out.print("username: ");
+            username = System.console().readLine();
+            System.out.print("password: ");
+            password = new String(System.console().readPassword());
+            isLoggedIn = checkCredentials(username, password);
+
+            if (!isLoggedIn) {
+                System.err.println("Incorrect username or password\n\n");
+                tryCount++;
+            }
+        } while (!isLoggedIn && tryCount < TRY_LIMIT);
+        if (isLoggedIn) {
+            System.out.printf("Successfully logged in as %s%n", username);
+            runCommandListener();
+        }
+        else
+            System.out.println("You have reached your attempts limit");
+        System.out.println("PROGRAM END");
+    }
+
+    /**
+     *
+     * @param username username input to validate
+     * @param password password input to validate
+     * @return
+     */
+    private static boolean checkCredentials(String username, String password) {
+        try {
+            PropertyHandler.load("/application-default.properties", "application.properties");
+            boolean areCredentialsValid = Objects.equals(username, PropertyHandler.getStringProperty(PROP_USERNAME)) &&
+                Objects.equals(password, PropertyHandler.getStringProperty(PROP_PASSWORD));
+            PropertyHandler.persist();
+            return areCredentialsValid;
+        } catch (Exception e) {
+            System.out.printf("%s: %s%n", e.getClass().getName(), e.getMessage());
+        }
+        return false;
+    }
+
+    // Funciton to listener a command a handler action to execute
+    private static void runCommandListener(){
+        String command;
+        do {
+            System.out.printf(">_ ");
+            command = System.console().readLine();
+            switch (command) {
+                case "ayuda":
+                    System.out.println("runngin ayuda");
+                    break;
+                case "exit":
+                    break;
+                default:
+                    System.err.printf("\"%s\" is not a recognized command%n", command);
+            }
+        } while (!"exit".equalsIgnoreCase(command));
+    }
 }
